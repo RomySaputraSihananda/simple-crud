39023711e59df30d2f9630adde94ada95c12283d
romys :) <romysaputrasihanandaa@gmail.com>
2024-04-02 12:58:46 +0700

    add test

---
+++ src/test/java/com/simplecrud/CrudDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = App.class)
+class CrudDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
